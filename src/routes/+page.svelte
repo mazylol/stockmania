5575dd9f5d22fe93b455dcca389fee65b914d363
mazylol <landonp95@gmail.com>
2023-05-12 15:28:35 -0400

    uwu

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,11 @@
 <p class="text-center text-2xl mt-4">A stock market game where you get to control the economy</p>
 
 <div class="flex justify-center mt-8">
-    <a href="/game" class="bg-green-400 hover:bg-green-500 text-white font-bold py-2 px-4 rounded">Play</a>
+    <a href="/game" class="bg-green-400 hover:bg-green-500 text-white font-bold py-2 px-4 rounded transition-colors duration-150">Play</a>
+</div>
+
+<div class="flex justify-center mt-2">
+    <a href="/about" class="bg-gray-400 hover:bg-gray-500 text-white font-bold py-2 px-4 rounded transition-colors duration-150">About</a>
 </div>
 
 <style>
