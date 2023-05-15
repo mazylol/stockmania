c7f75f46b95d3970a2f42ba3a79250f16123d4b2
mazylol <landonp95@gmail.com>
2023-05-14 23:27:45 -0400

    saves example data to file on first run

--- src-tauri/src/savedata.rs
+++ src-tauri/src/savedata.rs
@@ -1,12 +1,6 @@
-use serde::Deserialize;
+use std::{fs::File, io::Write};
 
-use std::fs::File;
-
-#[derive(Deserialize, Debug)]
-#[serde(rename_all = "camelCase")]
-pub struct SaveData {
-    
-}
+use serde::{Deserialize, Serialize};
 
 pub fn check_save() {
     let save_file = File::open("save.json");
@@ -15,6 +9,29 @@ pub fn check_save() {
         let new_save_file = File::create("save.json");
         if new_save_file.is_err() {
             println!("Failed to create a new save file!");
+        } else {
+            new_save_file.unwrap().write_all(serde_json::to_string(&Data {
+                stocks: Vec::new(),
+                balance: 10000.0,
+            })
+            .unwrap().as_bytes()).unwrap();
+
+            println!("New save file created!");
         }
     }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct Data {
+    stocks: Vec<Stock>,
+    balance: f64,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Stock {
+    name: String,
+    price: f64,
+    amount: i32,
+    total: f64,
 }
